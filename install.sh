d1b1b69ab65374865dd1a07a436a33e5dfd62432
Lee Findlow <lee@findlow.org.uk>
2015-03-03 19:48:24 +0000

    Configured install script to use a fixed set of files

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 src=~/dotfiles
-files=$(ls -1 $src | grep -v *.*)
+files=("vimrc" "gitconfig")
 
-for file in $files; do
+for file in ${files[@]}; do
     echo "Installing $file dotfile..."
     ln -sf $src/$file ~/.$file
 done
